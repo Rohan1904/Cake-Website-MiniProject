84137d3ddb6cd003665ff084a133ac932f11a3cd
Rohan1904 <51150171+Rohan1904@users.noreply.github.com>
2021-01-21 19:18:50 +0530

    validate login and forgot page

---
+++ js/login.js
@@ -0,0 +1,20 @@
+function validate() {
+    var uemail = document.getElementById("uemail").value;
+    var upassword = document.getElementById("upassword").value;
+
+
+    if (uemail == "") {
+        alert("Email cannot be empty");
+    }
+    if (upassword == "") {
+        alert("Password cannot be empty");
+    }
+}
+
+function validate2() {
+
+    var uemail = document.getElementById("uemail").value;
+    if (uemail == "") {
+        alert("Email cannot be empty");
+    }
+}
